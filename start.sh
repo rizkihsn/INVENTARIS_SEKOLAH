aea3748847ab8fc4e89d536aa3792c6f3350c14f
rizkihsn <rizkihasanfauzi36@gmail.com@example.com>
2025-07-13 10:19:22 +0700

    Fix: tambahkan init DB ke start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-gunicorn app:app
-git add start.sh
+python3 -c "
+from app import db, app
+from app import User
+from werkzeug.security import generate_password_hash
+
+with app.app_context():
+    db.create_all()
+    if not User.query.filter_by(username='admin').first():
+        admin = User(username='admin', password=generate_password_hash('admin123'), role='admin')
+        db.session.add(admin)
+        db.session.commit()
+"
+
+exec gunicorn app:app
